af26db2ebc6a2cf22a8b20c2343734e80821200b
Raul Veras <verasraul@gmail.com>
2024-09-17 10:23:56 -0400

    added function to fetch selected Pokemon's details onClick

--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -4,10 +4,13 @@ import { CLIENT_URL } from "../services/constants";
 import Pokemon from "./Pokemon";
 
 function PokeList() {
+    //  Each pokemon is an object so we need to use an empty array in our useState to take in multiple objects 
     //  Save API call to state
-    const [pokemons, setPokemons] = useState([]) // each pokemon is an object so we need to use an empty array in our useState to take in multiple objects 
+    const [pokemons, setPokemons] = useState([]); // Holds the list of Pokemon
+    const [selectedPokemonInfo, setSelectedPokemonInfo] = useState(null); // Holds the detailed data for selected Pokemon
 
-// create a function to handle our data using asynchronous code (will shoot data when it's loaded)
+// Create a function to handle our data using asynchronous code (will shoot data when it's loaded)
+// Functiont to hanlde fetching pokemon list
 const fetchData = async () => {
     // try to get the endpoint from the api
     try {
@@ -16,36 +19,68 @@ const fetchData = async () => {
         // destructore the data from the response
         const { results } = response.data;
         // set our state to the results (saving our API call to state)
-        setPokemons(results);
+        setPokemons(results); // Save the fetched Pokémon list to state
     // catch any error in the response and log that
     } catch (error) { 
         console.log(error);
     }
 }
-    // useEffect(): takes 2 arguments, a callback function and a depandency.
-    // the callback is argument is mandatory the dependancy is optional.
-    // SideNote: if you want to use an API call in ReactJS you HAVE TO use useEffect.
-    useEffect(() => {
-        // in order for our fetchdata function to work we have to wrap it around our useEffect.
-        // NEVER use 'async' inside useEffect as an argument/parameter, it has to be inside the body of the function.
-        fetchData();
-        // setting our dependancy argument.
-        // you can either set it to a dependancy that gets affected by the callback function or an empty array.
-        // in our case we need to use an empty array inside our useEffect as our second argument.
-        // the empty array causes the useEffect to run once, preventing it from running multiple times (or hit.ting infinite loop)
-    }, [])
+
+// Function to handle fetching Pokemon detials when clicked
+const fetchPokemonInfo = async (name) => {
+    console.log(name)
+    try { 
+        const response = await axios.get(`${CLIENT_URL}/${name}`); // Fetch Pokémon details from the provided URL
+        const results = response.data;
+        setSelectedPokemonInfo(results); // Save the fetched data to state
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// useEffect(): takes 2 arguments, a callback function and a depandency
+// The callback is argument is mandatory the dependancy is optional
+// SideNote: if you want to use an API call in ReactJS you HAVE TO use useEffect
+useEffect(() => {
+    // In order for our fetchdata function to work we have to wrap it around our useEffect
+    // NEVER use 'async' inside useEffect as an argument/parameter, it has to be inside the body of the function
+    fetchData(); // fetch pokemon list
+    //Ssetting our dependancy argument
+    // Youu can either set it to a dependancy that gets affected by the callback function or an empty array.
+    // In our case we need to use an empty array inside our useEffect as our second argument.
+    // The empty array causes the useEffect to run once, preventing it from running multiple times (or hit.ting infinite loop)
+}, [])
 
     return(
         <>
-        <ul className="container collection with-header" style={{ marginTop: 25 }}>
-            {/* map through the original state holding the data */}
-            {(pokemons || []).map((pokemon, index) => {
-                // deconstruct the name and url key from the pokemon argument for each pockemon object
-                const { name, url } = pokemon;
-                return (
-                <Pokemon name={name} url={url} key={index} />;
-            )})}
-        </ul>
+            <ul className="container collection with-header" style={{ marginTop: 25 }}>
+                {/* Map through the original state holding the Pokemon list */}
+                {(pokemons || []).map((pokemon, index) => {
+                    // deconstruct the name and url key from the pokemon argument for each pockemon object
+                    const { name, url } = pokemon;
+                    return (
+                    <Pokemon name={name} key={index} onClick={fetchPokemonInfo} />
+                    )}
+                )}
+            </ul>
+
+            {/* Render the selected Pokemon's detailed data if available */}
+            {selectedPokemonInfo && (
+                <div style={{marginTop: 25}}>
+                    <h2>Selected Pokemon Details</h2>
+                    <p><strong>Name:</strong> {selectedPokemonInfo.name}</p>
+                    <p><strong>Height:</strong> {selectedPokemonInfo.height}</p>
+                    <p><strong>Weight:</strong> {selectedPokemonInfo.weight}</p>
+                    <p><strong>Base Experience:</strong> {selectedPokemonInfo.base_experience}</p>
+                    <h3>Top Moves:</h3> 
+                    <ul>
+                        {selectedPokemonInfo.moves.map((moveobj, index) => (
+                            <li key={index}>{moveobj.move.name} </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+
         </>
     )
 }
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,13 +1,15 @@
 
 // because the information coming in is an object, we will use JSX within the arguments of the function
-function Pokemon({name, url}) {
+function Pokemon({name, url, onClick}) {
     return(
         // because our information is coming from an <ul> tag component 
         // we'll need to use the <li> tag to render data in this component, lists are nested under unordered lists in HTML
         <li className="collection-item">
             {/* we are going to create an anchor tag to make sure it toggles the data of pokemon */}
-            <a href={url}>{name}</a>
-
+            {/* Anchor tag to display the Pokemon name and trigger the click event */}
+            <a href="#!" onClick={() => onClick(name)}>
+                {name}
+            </a>
         </li>
     )
 }
